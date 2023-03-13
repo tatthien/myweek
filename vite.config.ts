3d759352fe1611d848ae9a59a3e5b0f0913fa1e6
Thien Nguyen <tatthien.contact@gmail.com>
2023-03-13 14:10:52 +0700

    feat: group components in the same chunk

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,14 @@ export default defineConfig({
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					auth: ['./src/pages/Signin.vue', './src/pages/Signup.vue'],
+					home: ['./src/pages/Home.vue'],
+				},
+			},
+		},
+	},
 })
