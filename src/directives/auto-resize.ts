6149994372bc46d85de3eb9256e8daf238fea5cc
Thien Nguyen <tatthien.contact@gmail.com>
2023-03-18 21:52:13 +0700

    fix: auto resize directive does not shrink properly

--- src/directives/auto-resize.ts
+++ src/directives/auto-resize.ts
@@ -3,15 +3,18 @@ export default {
 		const minHeight = binding.value || 0
 		el.style.height = `${el.scrollHeight > minHeight ? el.scrollHeight : minHeight}px`
 
-		el.addEventListener('input', () => {
-			el.style.height = minHeight
-			el.style.height = `${el.scrollHeight > minHeight ? el.scrollHeight : minHeight}px`
-		})
+		el.addEventListener(
+			'input',
+			() => {
+				el.style.height = 0
+				el.style.height = `${el.scrollHeight > minHeight ? el.scrollHeight : minHeight}px`
+			},
+			false
+		)
 	},
 	updated(el: Element, binding: any) {
+		const minHeight = binding.value || 0
 		if (el.value.trim() === '') {
-			const minHeight = binding.value || 0
-			el.style.height = minHeight
 			el.style.height = `${el.scrollHeight > minHeight ? el.scrollHeight : minHeight}px`
 		}
 	},
