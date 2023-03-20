9b8fa6ed18cc3be55e4f803ffa3ec43912ed13a2
Thien Nguyen <tatthien.contact@gmail.com>
2023-03-20 15:20:02 +0700

    chore: add logo to auth pages

--- src/pages/Signin.vue
+++ src/pages/Signin.vue
@@ -24,36 +24,42 @@ async function onSubmit() {
 </script>
 <template>
 	<div class="flex items-center justify-center h-full bg-gray-50">
-		<div class="mx-4 w-[400px] bg-white border border-gray-200 rounded-lg p-6">
-			<div class="mb-4">
-				<h1 class="text-xl font-medium">Welcome back</h1>
+		<div class="mx-4 w-[400px]">
+			<div class="mb-6 flex flex-col items-center justify-center">
+				<img src="/img/icon-192.png" alt="MyWeek Logo" class="w-12 mb-2" />
+				<div class="text-lg text-gray-600">MyWeek</div>
 			</div>
-			<FormKit type="form" :actions="false" :incomplete-message="false" novalidate @submit="onSubmit">
+			<div class="bg-white border border-gray-200 rounded-lg p-6">
 				<div class="mb-4">
-					<FormKit
-						v-model="form.email"
-						type="email"
-						label="Email"
-						placeholder="you@example.com"
-						validation="required|email"
-					/>
+					<h1 class="text-xl font-medium">Welcome back</h1>
 				</div>
-				<div class="mb-4">
-					<FormKit
-						v-model="form.password"
-						type="password"
-						label="Password"
-						placeholder="••••••••"
-						validation="required|length:6"
-					/>
+				<FormKit type="form" :actions="false" :incomplete-message="false" novalidate @submit="onSubmit">
+					<div class="mb-4">
+						<FormKit
+							v-model="form.email"
+							type="email"
+							label="Email"
+							placeholder="you@example.com"
+							validation="required|email"
+						/>
+					</div>
+					<div class="mb-4">
+						<FormKit
+							v-model="form.password"
+							type="password"
+							label="Password"
+							placeholder="••••••••"
+							validation="required|length:6"
+						/>
+					</div>
+					<div>
+						<WButton type="submit" full :loading="isSigningIn"> Sign in </WButton>
+					</div>
+				</FormKit>
+				<div class="text-center mt-8 text-sm">
+					<router-link to="/forgot-password" class="underline mr-4">Forgot password?</router-link>
+					<router-link to="/sign-up" class="underline"> Sign up now </router-link>
 				</div>
-				<div>
-					<WButton type="submit" full :loading="isSigningIn"> Sign in </WButton>
-				</div>
-			</FormKit>
-			<div class="text-center mt-8 text-sm">
-				<router-link to="/forgot-password" class="underline mr-4">Forgot password?</router-link>
-				<router-link to="/sign-up" class="underline"> Sign up now </router-link>
 			</div>
 		</div>
 	</div>
