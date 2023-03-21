ffd144576411843d9c5735d30d3aa4966009c46c
Thien Nguyen <tatthien.contact@gmail.com>
2023-03-21 22:14:09 +0700

    fix: reset password form is incorrect

--- src/pages/ResetPassword.vue
+++ src/pages/ResetPassword.vue
@@ -37,52 +37,6 @@ async function onSubmit() {
 			</div>
 			<div class="bg-white border border-gray-200 rounded-lg p-6">
 				<div class="mb-4">
-					<h1 class="text-xl font-medium">Get started</h1>
-				</div>
-				<FormKit type="form" :actions="false" :incomplete-message="false" novalidate @submit="onSubmit">
-					<div class="flex gap-3 mb-4">
-						<FormKit v-model="form.firstName" type="text" label="First name" placeholder="John" validation="required" />
-						<FormKit v-model="form.lastName" type="text" label="Last name" placeholder="Doe" validation="required" />
-					</div>
-					<div class="mb-4">
-						<FormKit
-							v-model="form.email"
-							type="email"
-							label="Email"
-							placeholder="you@example.com"
-							validation="required|email"
-						/>
-					</div>
-					<div class="mb-4">
-						<FormKit
-							v-model="form.password"
-							type="password"
-							label="Password"
-							name="password"
-							placeholder="••••••••"
-							validation="required|length:6"
-						/>
-					</div>
-					<div class="mb-4">
-						<FormKit
-							type="password"
-							label="Confirm password"
-							name="password_confirm"
-							placeholder="••••••••"
-							validation="required|confirm"
-						/>
-					</div>
-					<div>
-						<WButton type="submit" full :loading="isSigningUp"> Sign up </WButton>
-					</div>
-				</FormKit>
-				<div class="text-center text-sm mt-8">
-					<span>Have an account? </span>
-					<router-link to="/sign-in" class="underline"> Sign in now </router-link>
-				</div>
-			</div>
-			<div class="mx-4 w-[400px] bg-white border border-gray-200 rounded-lg p-6">
-				<div class="mb-4">
 					<h1 class="text-xl font-medium">Choose your new password</h1>
 				</div>
 				<FormKit type="form" :actions="false" :incomplete-message="false" novalidate @submit="onSubmit">
